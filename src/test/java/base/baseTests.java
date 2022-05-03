c76c72c3d1197e64d718beccd7e18fccba20776a
AmmarHussien <ammar.hussienmo97@gmail.com>
2022-05-03 21:41:42 +0200

    selenium_Java

--- src/test/java/base/baseTests.java
+++ src/test/java/base/baseTests.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
 
 public class baseTests {
@@ -13,13 +14,17 @@ public class baseTests {
     public void setUp() {
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.get("https://the-internet.herokuapp.com/");
+        goHome();
 
         homePage = new HomePage(driver);
     }
 
+    @BeforeMethod
+    public void goHome(){
+        driver.get("https://the-internet.herokuapp.com/");
+    }
     @AfterClass
     public  void tearDown(){
-        //driver.quit();
+        driver.quit();
     }
 }
